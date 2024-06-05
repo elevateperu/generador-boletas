157cc78f54d7f1b7e1726c76172b5d31ed32f027
cdanielgt <xt8733@rimac.com.pe>
2024-06-05 14:54:43 -0500

    loadiasdasdn     g

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://elevateperu.github.io/generador-boletas/'
+  base: '/generador-boletas/'
 })
